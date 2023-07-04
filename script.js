d2053dddfaa5895e16b40bf08c091952cbe6c595
JK <janke3567@gmail.com>
2023-07-04 14:09:16 +0200

    added h1 to screen-inner and css to place and style the text

---
+++ script.js
@@ -0,0 +1,24 @@
+let value1 = 0
+let operatorValue = ""
+let value2 = 0
+
+
+// A function that should take any amount of arguments and string them together with the given operators
+const operate = function (...args){
+    switch (operator) {
+        case '+' :
+            return number1 + number2;
+            break;
+        case '-':
+            return number1 - number2;
+            break;
+        case '*':
+            return number1 * number2
+        case '/':
+            return number1 / number2
+    }
+} 
+
+// Create a function that populates the display when clicking '='
+
+//
